64fc5e33af08cfba75065dc79d69e0b9dcdab1f2
MATTHEWSSM19 <smatthews4060@outlook.com>
2022-11-22 12:20:31 -0500

    Created some HTML forms for invite and signup

---
+++ src/prayer_group.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, render_template, flash, redirect, url_for
+from prayer_group_forms import InviteForm, SignupForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "weareprayergroup" #make secure
+
+@app.route("/")
+def hello():
+  return "hello"
+
+@app.get("/invite")
+def GET_invite():
+  iform = InviteForm()
+  return render_template("invite.j2", form=iform)
+
+@app.post("/invite")
+def POST_invite():
+  iform = InviteForm()
+  if iform.validate():
+    pass
+    # This is where you continue to signup below
+
+@app.get("/signup") #perhaps change route to "/signup"
+def GET_signup():
+    sform = SignupForm()
+    return render_template("signup.j2", form=sform) #add form from above
+
+@app.post("/signup") #perhaps change route to "/signup"
+def POST_signup():
+    sform = SignupForm()
+    if sform.validate():
+      pass
+      # This is where a new user is created and added to db
+        # temp = Student(
+        #     fname=sform.fname.data,
+        #     lname=sform.lname.data,
+        #     mintial=sform.mintial.data,
+        #     student_id=sform.student_id.data,
+        #     gender=sform.gender.data,
+        #     grad_year=sform.grad_year.data,
+        #     email=sform.email.data,
+        #     phone_num=sform.phone_num.data,
+        #     password=sform.password.data,
+        # )
+        # db.session.add(temp)
+        # db.session.commit()
+        # return redirect(url_for("main_page"))
+    else: #basic error handling
+        for field, error in sform.errors.items():
+            flash(f"{field}: {error}")
+        return redirect(url_for("GET_signup"))
+
+if __name__ == "__main__":
+  app.run()
---
+++ src/prayer_group_forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, SubmitField, PasswordField
+from wtforms.fields.html5 import EmailField, TelField
+from wtforms.validators import InputRequired, Email, Length, EqualTo
+
+class InviteForm(FlaskForm):
+    invite = StringField("Invitation Code: ", validators=[InputRequired("Please enter your Invitaton Code.")])
+    submit = SubmitField("Continue")
+
+class SignupForm(FlaskForm):
+    fname = StringField("First Name: ", validators=[InputRequired("Please enter your First Name.")])
+    lname = StringField("Last Name: ", validators=[InputRequired("Please enter your Last Name.")])
+    email = EmailField("Email Address: ", validators=[InputRequired("Please enter your Email.")])
+    password = PasswordField("Password: ", validators=[InputRequired("Please enter your Password."), Length(min=8, max=256)])
+    confirm = PasswordField("Confirm Password: ", validators=[InputRequired(), Length(min=8, max=256), EqualTo('password', message='Passwords must match.')])
+    phone_num = TelField("Phone Number (Optional): ")
+    home_address = StringField("Home Address (Optional): ")
+    submit = SubmitField("Sign Up")
